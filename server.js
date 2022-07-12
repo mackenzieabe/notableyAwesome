53afed8b82a569c30f9d515fcf6c75aa8785d2d4
mackgabe <mackenzieabe@gmail.com>
2022-07-11 20:24:23 -0400

    added app.get for notes and index html

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const {v4: uuidv4} = require('uuid')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let db = JSON.parse(data)
+        db.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+            if (err) throw err;
+            console.log('Saved a new note')
+        })
+    })
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log('App listening on http://localhost:' + PORT)
+})
